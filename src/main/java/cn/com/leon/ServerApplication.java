4cd03b73667fae08cb0f1f7e3ea6359e2db831f5
leon <18661230709@163.com>
2017-10-19 17:16:18 +0800

    Initial commit

---
+++ src/main/java/cn/com/leon/ServerApplication.java
@@ -0,0 +1,16 @@
+package cn.com.leon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author leon
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class,args);
+    }
+}
